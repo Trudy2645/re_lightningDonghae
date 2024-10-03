de8fa856ef3c9b98cdfecf1ef73000b11512dd8f
Trudy2645 <162314061+Trudy2645@users.noreply.github.com>
2024-10-03 12:07:12 +0900

    프젝생성

---
+++ re_lightningDonghae/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  re_lightningDonghae
+//
+//  Created by 트루디 on 10/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ re_lightningDonghae/re_lightningDonghaeApp.swift
@@ -0,0 +1,17 @@
+//
+//  re_lightningDonghaeApp.swift
+//  re_lightningDonghae
+//
+//  Created by 트루디 on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct re_lightningDonghaeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
